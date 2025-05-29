342e5c544ef8139c95ca6dbc86fbaad4d7daec87
hervekwizera <hervekwizera63@gmail.com>
2025-05-29 07:23:32 +0200

    modification of auth.controllers

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,33 +1,26 @@
 import mongoose from "mongoose";
-import bcrypt from "bcryptjs";
-import  jwt from 'jsonwebtoken'
-import User from '../models/user.model.js'
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
 import { JWT_SECRET, JWT_EXPIRES_IN } from "../config/env.js";
 
-
-// what is a req body? => req.body is an object containing data from  the client (post request)
-
-export const register = async (req,res,next)=>{
-    const  session = await mongoose.startSession();
+export const register = async (req, res, next) => {
+    const session = await mongoose.startSession();
     session.startTransaction();
 
     try {
-        const {name,email,password} = req.body;
-        // check if a user already exists
-        const existingUser = await User.findOne({email});
+        const { name, email, password } = req.body;
 
-        if(existingUser){
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
             const error = new Error('User already exists');
             error.statusCode = 409;
             throw error;
         }
-        // hash password
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password,salt);
 
-        const newUsers = await User.create([{name,email,password:hashedPassword}],{session})
+        const user = new User({ name, email, password });
+        await user.save({ session });
 
-        const token = jwt.sign({userId:newUsers[0]._id},JWT_SECRET,{expiresIn: JWT_EXPIRES_IN})
+        const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
 
         await session.commitTransaction();
         session.endSession();
@@ -35,11 +28,11 @@ export const register = async (req,res,next)=>{
         res.status(201).json({
             success: true,
             message: 'User created successfully',
-            data:{
+            data: {
                 token,
-                user: newUsers[0],
+                user,
             }
-        })
+        });
     } catch (error) {
         await session.abortTransaction();
         session.endSession();
@@ -47,36 +40,44 @@ export const register = async (req,res,next)=>{
     }
 };
 
-export const login = async (req,res,next)=>{
+export const login = async (req, res, next) => {
     try {
-        const {email,password} = req.body;
-        const user = await User.findOne({email});
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
 
-        if (!user){
-             const error = new Error('User not found');
-             error.statusCode = 404;
-             throw error;
+        if (!user) {
+            const error = new Error('User not found');
+            error.statusCode = 404;
+            throw error;
         }
 
-        const isPasswordValid = await bcrypt.compare(password, user.password);
-        if(!isPasswordValid){
+        const isPasswordValid = await user.matchPassword(password);
+        if (!isPasswordValid) {
             const error = new Error('Invalid password');
             error.statusCode = 401;
             throw error;
         }
-        const token = jwt.sign({userId: user._id},JWT_SECRET,{expiresIn: JWT_EXPIRES_IN});
+
+        const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
+
         res.status(200).json({
             success: true,
             message: 'User signed in successfully',
-            data:{
+            data: {
                 token,
                 user,
             }
-        })
+        });
 
     } catch (error) {
         next(error);
     }
 };
 
-export const signOut = async (req,res,next)=>{};
+export const signOut = async (req, res, next) => {
+    // Optional: You can blacklist JWT or remove it from the client
+    res.status(200).json({
+        success: true,
+        message: "User signed out successfully",
+    });
+};
