0d8c8fa042dbf3e57e050267e2f12f33eefe72ff
hervekwizera <hervekwizera63@gmail.com>
2025-08-20 22:23:04 +0200

    modification of json

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,11 @@
 // server.js
 import express from "express";
+import dotenv from "dotenv";
 
+
+dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Test route
 app.get("/", (req, res) => {
