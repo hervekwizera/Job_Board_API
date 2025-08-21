16a33e92e25a0ed93b72b57f3bc184f5733bd8b7
hervekwizera <hervekwizera63@gmail.com>
2025-08-21 22:17:51 +0200

    updating server.js by adding cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,16 @@
 // server.js
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json()); // Parse JSON
+app.use(express.urlencoded({ extended: true })); // Parse form data
+app.use(cors()); // Enable CORS
 
 // Test route
 app.get("/", (req, res) => {
