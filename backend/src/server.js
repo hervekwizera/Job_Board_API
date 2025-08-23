c5383bb80c1ec13a23395abb08c1635f446f8f46
hervekwizera <hervekwizera63@gmail.com>
2025-08-23 22:26:53 +0200

    setup db config

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,16 +2,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import connectDB from "./config/db.js";
 
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import jobRoutes from "./routes/job.routes.js";
 
-
-
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Connect to MongoDB
+connectDB();
 
 app.use(express.json()); // Parse JSON
 app.use(express.urlencoded({ extended: true })); // Parse form data
@@ -21,7 +23,6 @@ app.use(cors()); // Enable CORS
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/jobs", jobRoutes);
-
 
 // Test route
 app.get("/", (req, res) => {
